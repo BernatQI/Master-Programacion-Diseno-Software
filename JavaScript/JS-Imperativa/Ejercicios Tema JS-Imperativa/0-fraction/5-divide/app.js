567270d015090fd9806f0c47e0a0f90591536576
BernatQI <bernatpoca@gmail.com>
2022-08-09 10:32:12 +0200

    latest

---
+++ JavaScript/JS-Imperativa/Ejercicios Tema JS-Imperativa/0-fraction/5-divide/app.js
@@ -0,0 +1,12 @@
+const { Console } = require("console-mpds");
+const console = new Console();
+
+console.writeln(`Primera fracción:`);
+const numerator1 = console.readNumber(`Introduce el numerador de la fracción:`);
+const denominator1 = console.readNumber(`Introduce el denominador de la fracción:`);
+
+console.writeln(`Segunda fracción:`);
+const numerator2 = console.readNumber(`Introduce el numerador de la fracción:`);
+const denominator2 = console.readNumber(`Introduce el denominador de la fracción:`);
+
+console.writeln(`La suma de la fracción ${numerator1}/${denominator1} y la fracción ${numerator2}/${denominator2} es la fracción ${numerator1 * denominator2}/${numerator2 * denominator1}`);
