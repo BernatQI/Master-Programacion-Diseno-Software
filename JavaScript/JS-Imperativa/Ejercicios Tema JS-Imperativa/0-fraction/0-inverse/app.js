9b7c4aa940e14836e717d373302744c89b085d07
BernatQI <bernatpoca@gmail.com>
2022-08-09 07:49:30 +0200

    latest

--- JavaScript/JS-Imperativa/Ejercicios Tema JS-Imperativa/0-fraction/0-inverse/app.js
+++ JavaScript/JS-Imperativa/Ejercicios Tema JS-Imperativa/0-fraction/0-inverse/app.js
@@ -4,4 +4,4 @@ const console = new Console();
 const numerador = console.readNumber('Introduce el numerador de la fracción:');
 const denominador = console.readNumber('Introduce el denominador de la fracción:');
 
-console.writeln(`La fracción ${numerador}/${denominador} invertida es la fracción ${denominador}/${numerador}`)
+console.writeln(`La fracción ${numerador}/${denominador} invertida es la fracción ${denominador}/${numerador}`);
