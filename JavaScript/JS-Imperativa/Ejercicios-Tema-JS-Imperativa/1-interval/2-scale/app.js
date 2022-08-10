108ddd1ea62e4f2cbaf049f5ac42c8d5d9951475
BernatQI <bernatpoca@gmail.com>
2022-08-10 08:09:30 +0200

    latest

--- JavaScript/JS-Imperativa/Ejercicios-Tema-JS-Imperativa/1-interval/2-scale/app.js
+++ JavaScript/JS-Imperativa/Ejercicios-Tema-JS-Imperativa/1-interval/2-scale/app.js
@@ -3,4 +3,9 @@ const console = new Console();
 
 const minInterval = console.readNumber('Introduce el mínimo del intervalo:');
 const maxInterval = console.readNumber('Introduce el máximo del intervalo (superior o igual al mínimo):');
-const factor = console.readNumber('Introduce un factor de escala positivo:');
+const scale = console.readNumber('Introduce un factor de escala positivo:');
+
+const long = (maxInterval - minInterval) / 2;
+const midPoint = maxInterval - long;
+
+console.writeln(`El intervalo [${minInterval}, ${maxInterval}] con factor de escala ${scale} es el intervalo [${midPoint - (long * scale)}, ${midPoint + (long * scale)}]`);
