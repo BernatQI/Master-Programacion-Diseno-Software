ce761cff814e8a1efe04757c68152d101258250c
BernatQI <bernatpoca@gmail.com>
2022-08-10 21:29:59 +0200

    latest

--- JavaScript/JS-Imperativa/Ejercicios-Tema-JS-Imperativa/2-coordinate/1-quadrant/app.js
+++ JavaScript/JS-Imperativa/Ejercicios-Tema-JS-Imperativa/2-coordinate/1-quadrant/app.js
@@ -5,8 +5,8 @@ const abcissa = console.readNumber('Dame la abcisa de la coordenada:');
 const ordinate = console.readNumber('Dame la ordenada de la coordenada:');
 
 const firstQuadrant = abcissa >= 0 && ordinate >= 0 ? '1' : '';
-const secondQuadrant = abcissa >= 0 && ordinate <= 0 ? '2' : '';
+const secondQuadrant = abcissa <= 0 && ordinate >= 0 ? '2' : '';
 const thirdQuadrant = abcissa <= 0 && ordinate <= 0 ? '3' : '';
-const fourthQuadrant = abcissa <= 0 && ordinate >= 0 ? '4' : '';
+const fourthQuadrant = abcissa >= 0 && ordinate <= 0 ? '4' : '';
 
 console.writeln(`La coordenada (${abcissa}, ${ordinate}) está en el ${firstQuadrant}${secondQuadrant}${thirdQuadrant}${fourthQuadrant}º cuadrante.`);
