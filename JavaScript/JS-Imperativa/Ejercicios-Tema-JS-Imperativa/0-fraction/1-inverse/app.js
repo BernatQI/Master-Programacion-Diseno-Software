c53b2ed7debe392ea438d3f3b69ea3159fef838a
BernatQI <bernatpoca@gmail.com>
2022-08-14 12:14:31 +0200

    latest

--- JavaScript/JS-Imperativa/Ejercicios-Tema-JS-Imperativa/0-fraction/1-inverse/app.js
+++ JavaScript/JS-Imperativa/Ejercicios-Tema-JS-Imperativa/0-fraction/1-inverse/app.js
@@ -13,9 +13,7 @@ for(let i = 2; i <= numerator; i++) {
     }else if (numerator % i == 0 && i == numerator) {
         mcdNumerator += [`${i}`];
     }
-}
 
-for(let i = 2; i <= denominator; i++) {
     if(denominator % i == 0 && i < denominator){
         mcdDenominator += [`${i},`];
     }else if (denominator % i == 0 && i == denominator) {
