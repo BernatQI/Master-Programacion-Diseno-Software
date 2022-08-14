6e60d5fe829b4f1bd7a7eb3a4f1b89c056393bb9
BernatQI <bernatpoca@gmail.com>
2022-08-14 12:12:07 +0200

    latest

--- JavaScript/JS-Imperativa/Ejercicios-Tema-JS-Imperativa/0-fraction/1-inverse/app.js
+++ JavaScript/JS-Imperativa/Ejercicios-Tema-JS-Imperativa/0-fraction/1-inverse/app.js
@@ -3,3 +3,33 @@ const console = new Console();
 
 const numerator = console.readNumber(`Introduce el numerador de la fracción:`);
 const denominator = console.readNumber(`Introduce el denominador de la fracción:`);
+
+let mcdNumerator = [];
+let mcdDenominator = [];
+
+for(let i = 2; i <= numerator; i++) {
+    if(numerator % i == 0 && i < numerator){
+        mcdNumerator += [`${i},`];
+    }else if (numerator % i == 0 && i == numerator) {
+        mcdNumerator += [`${i}`];
+    }
+}
+
+for(let i = 2; i <= denominator; i++) {
+    if(denominator % i == 0 && i < denominator){
+        mcdDenominator += [`${i},`];
+    }else if (denominator % i == 0 && i == denominator) {
+        mcdDenominator += [`${i}`];
+    }
+}
+
+let mcd = 1;
+for(let i = 0; i < mcdNumerator.length; i++) {
+    for(let j = 0; j < mcdDenominator.length; j++) {
+        if(mcdNumerator[i] == mcdDenominator[j]) {
+            mcd *= mcdNumerator[i];
+        }
+    }
+}
+
+console.writeln(`La fracción ${numerator}/${denominator} = ${numerator/mcd}/${denominator/mcd} invertida es la fracción ${denominator/mcd}/${numerator/mcd}`);
---
+++ JavaScript/JS-Imperativa/Ejercicios-Tema-JS-Imperativa/3-numbers/6-prime/v1.0/README.md
@@ -0,0 +1,4 @@
+~~~~
+Dame un número positivo:  12
+La suma de los primos en los primeros 12 números es 28
+~~~~
