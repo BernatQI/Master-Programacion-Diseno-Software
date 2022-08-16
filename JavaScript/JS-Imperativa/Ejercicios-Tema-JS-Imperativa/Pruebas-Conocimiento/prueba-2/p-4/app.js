c60ee5aa07362921d66fc814caa0583a1510aaf4
BernatQI <bernatpoca@gmail.com>
2022-08-16 10:34:01 +0200

    latest

---
+++ JavaScript/JS-Imperativa/Ejercicios-Tema-JS-Imperativa/Pruebas-Conocimiento/prueba-2/p-4/app.js
@@ -0,0 +1,29 @@
+const { Console } = require("console-mpds");
+const console = new Console();
+
+const rowOut = '* * * * *';
+const rowIn = '*.......*';
+
+const columns = console.readNumber(`¿Cuántas columnas quieres?:`);
+const rows = console.readNumber(`¿Cuántas filas quieres?:`);
+
+let row0 = '';
+let row1 = '';
+let row2 = '';
+let row3 = '';
+let row4 = '';
+for(let i = 0; i < columns; i++) {
+    row0 += rowOut;
+    row1 += rowIn;
+    row2 += rowIn;
+    row3 += rowIn;
+    row4 += rowOut;
+}
+
+for(let i = 0; i < rows; i++) {
+    console.writeln(`${row0}
+${row1}
+${row2}
+${row3}
+${row4}`);
+}
