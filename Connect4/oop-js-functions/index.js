3a711d59414a0c5f02c364280c9e8e7fcc8e0731
BernatQi <bernatqipro@gmail.com>
2023-10-23 19:58:43 +0000

    update

--- Connect4/oop-js-functions/index.js
+++ Connect4/oop-js-functions/index.js
@@ -1,40 +1,88 @@
 const { Console } = require("console-mpds");
 const console = new Console();
 
-const c4 = connect4();
-c4.init();
 
 function connect4() {
-  const game = game();
-  const gameView = gameView();
+  const game = Game();
+  const gameView = GameView();
 
   return {
     init() {
-      do {
-        console.writeln(gameView.head);
-      }while(game.isOver());
-    },
+      console.writeln(gameView.MESSAGES.head);
 
-    playAgain() {
-      this.init();
+      do {
+        game.play();
+      }while (gameView.playAgain());
+      console.writeln('Bye!');
     }
   }
 }
 
-function game() {
+const c4 = connect4();
+c4.init();
+
+function Game() {
+  let board = Board();
+  const boardView = BoardView();
 
   return {
+    play() {
+      GameBoard = board.reset();
+      do {
+        boardView.show(GameBoard);
+      }while (this.isOver());
+    },
     isOver() {
       return false;
     }
   }
 }
 
-function gameView() {
+function GameView() {
 
   return {
     MESSAGES: {
       head: `\t---> CONNECT 4 <---\n`
+    },
+    playAgain() {
+      let answer;
+
+      do {
+        answer = console.readString('Play again? (answer "yes" or "no"):');
+      } while (!this.isValidAnswer(answer));
+
+      return answer === 'yes';
+    },
+    isValidAnswer(answer) {
+      return answer === 'yes' || answer === 'no';
+    }
+  }
+}
+
+function Board() {
+  const NUMBER_ROWS = 6;
+  const delimeter = ' | ';
+  const empty = '_';
+  let board;
+
+  return {
+    reset() {
+      board = [];
+      for (let i = 0; i < NUMBER_ROWS; i++) {
+        board[i] = [`${i + 1}`, `${delimeter + empty}`, `${delimeter + empty}`, `${delimeter + empty}`, `${delimeter + empty}`, `${delimeter + empty}`, `${delimeter + empty}`, `${delimeter + empty + delimeter} \n`];
+      }
+      return board.reverse();
+    }
+  }
+}
+
+function BoardView() {
+
+  return {
+    show(board) {
+      for (const row of board) {
+        console.writeln(row.join(''));
+      }
     }
   }
 }
